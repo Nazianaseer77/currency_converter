c06b176b83d8d65191737aa1206784cdc9d03b0a
Nazianaseer77 <160523698+Nazianaseer77@users.noreply.github.com>
2024-05-18 16:43:45 +0500

    Add files via upload
    currency converter project

---
+++ app.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+
+let exchange_rate: any ={
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 111,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 280,
+}
+
+let user_answer = await inquirer.prompt([
+{
+    name:"from_currency",
+    type:"list",
+    message:"select the currency to convert from.",
+    choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+},
+{
+    name:"to_currency",
+    type:"list",
+    message: "select the the currency to convert into:",
+    choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+},
+{
+    name:"amount",
+    type:"input",
+    message:"enter the amount to convert:",
+
+}
+
+]);
+
+// perform curency conversion by using formula
+
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount
+
+//formula of conversion
+
+let base_amount= amount / from_amount
+let converted_amount = base_amount * to_amount
+
+//display the converted amount
+
+console.log(`converted amount ${converted_amount}`);
